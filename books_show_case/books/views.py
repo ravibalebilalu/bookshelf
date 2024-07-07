67a88391d759d988222a3521f31140b3ef2d85ed
ravibalebilalu <81ravikiran@gmail.com>
2024-07-07 13:35:16 +0530

    authors

--- books_show_case/books/views.py
+++ books_show_case/books/views.py
@@ -4,10 +4,18 @@ from django.template import loader
 from books.models import Book
 
 def index(request):
-    book = Book.objects.all()
+    book = Book.objects.all()[:20]
 
     context={
         "book":book
     }
     template = loader.get_template("books/home.html")
+    return HttpResponse(template.render(context,request))
+
+def authors(request):
+    books = Book.objects.values('bookauthors').distinct().order_by("bookauthors")
+    template = loader.get_template("books/authors.html")
+    context = {
+        "books":books
+    }
     return HttpResponse(template.render(context,request))
--- books_show_case/data_update.py
+++ books_show_case/data_update.py
@@ -3,47 +3,45 @@ import csv
 import django
 from datetime import datetime
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'books_show_case.settings')
 django.setup()
 
- 
+print("Shrekrishna................")
+
 def parse_date(date_str):
     try:
-        return datetime.strptime(date_str,"%m/%d/%Y").date()
-    except  ValueError:
-        return None
-
-
+        return datetime.strptime(date_str, "%m/%d/%Y").date()
+    except ValueError:
+        return None  # Handle invalid dates
 
 def data_from_csv(file_path):
     from books.models import Book
-    with open(file_path,"r")as file:
+    with open(file_path, "r") as file:
         csv_reader = csv.DictReader(file)
 
         for row in csv_reader:
             publication_date = parse_date(row["publication_date"])
             if publication_date is None:
+                print(f"Invalid date format for book {row['title']}: {row['publication_date']}")
                 continue
-            book_details = Book(
-                bookid =  row["bookID"],
-                booktitle =  row["title"],
-                bookauthors = row["authors"],
-                bookrating =  row["average_rating"],
-                bookisbn =  row["isbn"],
-                bookisbn13 =  row["isbn13"],
-                book_language_code =  row["language_code"],
-                bookpages =  row["num_pages"],
-                book_rating_counts =  row["ratings_count"],
-                book_text_reviews_count =  row["text_reviews_count"],
-                book_publication_date =  publication_date,
-                book_publisher =  row["publisher"]
-                                                    )
-            
-            book_details.save()
-            print("Saving....")
 
+            book_details = Book(
+                bookid=row["bookID"],
+                booktitle=row["title"],
+                bookauthors=row["authors"],
+                bookrating=row["average_rating"],
+                bookisbn=row["isbn"],
+                bookisbn13=row["isbn13"],
+                book_language_code=row["language_code"],
+                bookpages=row["num_pages"],
+                book_rating_counts=row["ratings_count"],
+                book_text_reviews_count=row["text_reviews_count"],
+                book_publication_date=publication_date,
+                book_publisher=row["publisher"]
+            )
+
+            book_details.save()
+            print(f"Saving {row['title']}....")
 
 file_path = "../data/books.csv"
 data_from_csv(file_path)
- 
