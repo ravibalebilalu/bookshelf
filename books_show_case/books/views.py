63a17f435d3b9a2c8635d897705d5d80b0b829d4
ravibalebilalu <81ravikiran@gmail.com>
2024-07-11 13:51:18 +0530

    authors

--- books_show_case/books/views.py
+++ books_show_case/books/views.py
@@ -3,28 +3,30 @@ from django.http import HttpResponse
 from django.template import loader
 from books.models import Book
 from datetime import datetime
+from collections import defaultdict
+
 
 def index(request):
-    book = Book.objects.all()[:20]
-
-    context={
+    book = Book.objects.all()
+    context = {
         "book":book
     }
     template = loader.get_template("books/home.html")
     return HttpResponse(template.render(context,request))
 
 def authors(request):
-    template = loader.get_template("books/authors.html")
-    authors = Book.objects.values('bookauthors').distinct().order_by("bookauthors")
-    
+    books = Book.objects.all().order_by('bookauthors')
+    author_books = defaultdict(set)  # Using a set to avoid duplicates
 
-
-
+    for book in books:
+        authors_list = book.bookauthors.split('/')  # Assuming authors are separated by '/'
+        for author in authors_list:
+            author_books[author.strip()].add(book.booktitle)
     
     context = {
-        "authors":authors
+        "authors": [{"bookauthors": author, "booktitles": list(titles)} for author, titles in author_books.items()]
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, "books/authors.html", context)
 
 """
     bookid booktitle bookauthors bookrating bookisbn bookisbn13 book_language_code bookpages book_rating_counts 
